183af917ca91c67709c25b55fefdf40e84fe6d2e
Annessa Bock-Willmes <annessa.hartey@hotmail.com>
2018-11-29 04:28:10 +0000

    fixed typo in migrate db with rollback, added places to database via console, updated index

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2018_11_28_061519) do
 
   create_table "places", force: :cascade do |t|
     t.string "name"
-    t.text "descriptiom"
+    t.text "description"
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
