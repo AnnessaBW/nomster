b01c61205b241a667e5160218b1e3e7325bcedbd
Annessa Bock-Willmes <annessa.hartey@hotmail.com>
2018-12-22 16:08:23 +0000

    update domain for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
     authentication: :plain,
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
+    domain: 'https://nomster-annessa.herokuapp.com',
     enable_starttls_auto: true
   }
 
